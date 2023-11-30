fb87be7029784c510f2e4c56d1e8dc986957e0cd
QAJ123 <josmyjosekpl@gmail.com>
2023-11-30 16:35:05 +0530

    changed groovy file

--- .idea/test1.groovy
+++ .idea/test1.groovy
@@ -9,13 +9,13 @@ node {
         git url: 'https://github.com/QAJ123/JenkinsIntegration.git'
     }
 
-    stage('Artifactory configuration') {
+ /*   stage('Artifactory configuration') {
         // Tool name from Jenkins configuration
         rtMaven.tool = "Maven-3.9.5"
         // Set Artifactory repositories for dependencies resolution and artifacts deployment.
         rtMaven.deployer releaseRepo:'libs-release-local', snapshotRepo:'libs-snapshot-local', server: server
         rtMaven.resolver releaseRepo:'libs-release', snapshotRepo:'libs-snapshot', server: server
-    }
+    }*/
 
     stage('Maven build') {
         buildInfo = rtMaven.run pom: 'maven-example/pom.xml', goals: 'clean test'
